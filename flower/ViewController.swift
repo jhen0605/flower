3b930040d50dc6f6a3bf310a1632f6008ad27cf4
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-05-26 19:25:51 +0800

    1

--- flower/ViewController.swift
+++ flower/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var flowerButton: UIButton!
+    @IBOutlet weak var showLabel: UILabel!
+    @IBOutlet weak var restartButton: UIButton!
+    
+    var ans: Int?
+    var count = 0
+    var showBool = true
+    
+    let speechSynth = AVSpeechSynthesizer()
+    let startUtterance = AVSpeechUtterance(string: "Click here")
+    let loveUtterance1 = AVSpeechUtterance(string: "love me")
+    let loveUtterance2 = AVSpeechUtterance(string: "Don't love me")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        flowerButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+        ans = Int.random(in: 1...10)
+        print(ans!)
+        
+        startUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        loveUtterance1.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        loveUtterance2.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        speechSynth.speak(startUtterance)
     }
 
-
+    @IBAction func pressFlowerButton(_ sender: Any) {
+        count += 1
+        if count == ans {
+            flowerButton.isHidden = true
+        }
+        if showBool {
+            flowerButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi * 5 / 8)
+            showLabel.text = "love me"
+            showBool.toggle()
+            speechSynth.speak(loveUtterance1)
+        } else {
+            flowerButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi * 3 / 8)
+            showLabel.text = "Don't love me"
+            showBool.toggle()
+            speechSynth.speak(loveUtterance2)
+        }
+    }
+    
+    @IBAction func pressRestartButton(_ sender: Any) {
+        flowerButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+        ans = Int.random(in: 1...10)
+        showLabel.text = "Click here"
+        showBool = true
+        flowerButton.isHidden = false
+        count = 0
+        print(ans!)
+        speechSynth.speak(startUtterance)
+    }
+    
+    
 }
 
